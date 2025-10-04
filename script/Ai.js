f4be05e0738111164f13854fda1dc1202e649c00
davbotapp <davbotapp@gmail.com>
2025-10-04 12:51:50 +0100

    Update Ai.js

--- script/Ai.js
+++ script/Ai.js
@@ -1,30 +1,28 @@
+
 const axios = require('axios');
 
 module.exports.config = {
-		name: 'ai',
-		version: '1.0.0',
-		role: 0,
-		hasPrefix: false,
-		description: "An AI command powered by OpenAI",
-		usages: "",
-		credits: 'Developer',
-		cooldown: 5,
+  name: 'ai',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['gpt', 'openai', 'assistant'],
+  description: "Une commande IA avec l'API Hercai",
+  usage: "ai [question]",
+  credits: 'David Mpongo',
+  cooldown: 3,
 };
 
 module.exports.run = async function({ api, event, args }) {
-		if (!args[0]) {
-				api.sendMessage("❓| Pose ta question à Davbot 0.4 🤓", event.threadID);
-				return;
-		}
+  const question = args.join(" ");
+  if (!question) return api.sendMessage("❌ Veuillez entrer une question.\nExemple : /ai Bonjour !", event.threadID, event.messageID);
 
-		const question = args.join(" ");
-		const apiUrl = `https://openai-rest-api.vercel.app/hercai?ask=${encodeURIComponent(question)}&model=v3`;
+  try {
+    const res = await axios.get(`https://openai-rest-api.vercel.app/hercai?ask=encodeURIComponent(question)   model=v3`);
+    const reply = res.data.reply;
 
-		try {
-				const response = await axios.get(apiUrl);
-				api.sendMessage(response.data.reply, event.threadID);
-		} catch (error) {
-				console.error("Error fetching response from OpenAI API:", error);
-				api.sendMessage("An error occurred while processing your request. Please try again later.", event.threadID);
-		}
+    api.sendMessage(`🤖 Davbot :{reply}`, event.threadID, event.messageID);
+  } catch (err) {
+    api.sendMessage("❌ Erreur lors de la requête à l'IA.", event.threadID, event.messageID);
+  }
 };
